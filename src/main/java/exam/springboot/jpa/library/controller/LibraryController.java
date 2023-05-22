09f6eff81e63298b07eef6764c4d761b45673fac
zzyzigy <zzyzigy@gmail.com>
2023-05-22 17:55:05 +0900

    controller 에서 Library 서비스 호출

--- src/main/java/exam/springboot/jpa/library/controller/LibraryController.java
+++ src/main/java/exam/springboot/jpa/library/controller/LibraryController.java
@@ -1,14 +1,27 @@
 package exam.springboot.jpa.library.controller;
 
+import exam.springboot.jpa.library.service.LibraryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LibraryController {
 
+    @Autowired
+    LibraryService lbsrv;
+
     @GetMapping("/")
-    public String list() {
-        return "list";
+    public ModelAndView list(Integer cpg) {
+        ModelAndView mv = new ModelAndView();
+
+        if (cpg == null || cpg == 0) cpg = 1;
+        mv.addObject("lblist", lbsrv.readLibrary(cpg));
+
+        mv.setViewName("list");
+
+        return mv;
     }
 
 }
