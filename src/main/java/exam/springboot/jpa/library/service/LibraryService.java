8b9ca9ee941efad01a44d7ba182cf8a40884765a
zzyzigy <zzyzigy@gmail.com>
2023-05-22 17:55:37 +0900

    Library Service 인터페이스, 클래스 구현

---
+++ src/main/java/exam/springboot/jpa/library/service/LibraryService.java
@@ -0,0 +1,11 @@
+package exam.springboot.jpa.library.service;
+
+import exam.springboot.jpa.library.model.Library;
+
+import java.util.List;
+
+public interface LibraryService {
+
+    List<Library> readLibrary(int cpg);
+
+}
---
+++ src/main/java/exam/springboot/jpa/library/service/LibraryServiceImpl.java
@@ -0,0 +1,21 @@
+package exam.springboot.jpa.library.service;
+
+import exam.springboot.jpa.library.dao.LibraryDAO;
+import exam.springboot.jpa.library.model.Library;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("lbsrv")
+public class LibraryServiceImpl implements LibraryService {
+
+    @Autowired
+    LibraryDAO lbdao;
+
+    @Override
+    public List<Library> readLibrary(int cpg) {
+        return lbdao.selectLibrary(cpg - 1);
+    }
+
+}
